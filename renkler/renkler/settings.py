2c63607f9b934b39e0cf45b8eb58eecbb7328ced
UgurcanAkpulat <ugurcan.akpulat@eleena.nl>
2021-01-22 23:54:02 +0100

    anan

--- renkler/renkler/settings.py
+++ renkler/renkler/settings.py
@@ -26,10 +26,15 @@ SECRET_KEY = 'fy@d@6!=@e--2uo4@!ajmuh0s@+zn#ym%tzs3%!t2!vd1sv@r('
 DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+
+CORS_ORIGIN_WHITELIST = (	
+       'http://9193451665a1.ngrok.io/',
+       'macukadam.pythonanywhere.com'
+)
 
 
-ALLOWED_HOSTS = ['macukadam.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['macukadam.pythonanywhere.com', '127.0.0.1', 'http://9193451665a1.ngrok.io/']
+
 
 
 # Application definition
@@ -50,7 +55,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
