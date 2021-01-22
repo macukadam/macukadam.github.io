2c0b68d24c3284cbc158f95828bdc27dd9e70edc
UgurcanAkpulat <ugurcan.akpulat@eleena.nl>
2021-01-23 00:10:51 +0100

    lelele

--- renkler/renkAPI/views.py
+++ renkler/renkAPI/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-import random
+import random, json
 from rest_framework.decorators import api_view
 
 
@@ -8,18 +8,20 @@ COLORS = [{'color':'White', 'hex':'#FFFFFF'},
             {'color':'Gray', 'hex':'#808080'}, 
             {'color':'Black', 'hex':'#000000'}]
 
-CORRECT_COLOR = ''
 
 @api_view(['GET'])
 def chose_color(request):
-    CORRECT_COLOR =random.choice(COLORS)['color']
-    return JsonResponse({'chosen_color':CORRECT_COLOR})
+    request.session['CORRECT_COLOR'] =random.choice(COLORS)['color']
+    return JsonResponse({'chosen_color':request.session['CORRECT_COLOR']})
 
 @api_view(['POST', 'GET'])
 def index(request):
     if request.method == 'POST':
-        color = request.POST.get('color')['color']
-        if (color == CORRECT_COLOR):
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        color = body['color']
+        # color = request.POST.get('color')['color']
+        if (color == request.session.get('CORRECT_COLOR')):
             return JsonResponse({'result':'correct'})
         else:
             return JsonResponse({'result':'incorrect'})
